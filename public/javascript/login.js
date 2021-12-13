f916251bb3079ec27b609133ef7182367c153455
Mark Granade <88593585+MarkGranade@users.noreply.github.com>
2021-12-13 13:43:40 -0500

    added login logic
    login validation is complete

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,27 @@
+async function loginFormHandler(event) {
+	event.preventDefault();
+
+	const email = document.querySelector("#email-login").value.trim();
+	const password = document.querySelector("#password-login").value.trim();
+
+	if (email && password) {
+		const response = await fetch("/api/users/login", {
+			method: "post",
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+			headers: { "Content-Type": "application/json" },
+		});
+
+		if (response.ok) {
+			document.location.replace("/");
+		} else {
+			alert(response.statusText);
+		}
+	}
+}
+
 async function signupFormHandler(event) {
 	event.preventDefault();
 
@@ -6,7 +30,9 @@ async function signupFormHandler(event) {
 	const password = document.querySelector("#password-signup").value.trim();
 
 	if (username && email && password) {
-		await fetch("/api/users", {
+		// when using 'await' we can assign the result of the promise to a variable
+		// This way, we don't need to use 'catch()' or 'then()' to tell the code what to do after the Promise
+		const response = await fetch("/api/users", {
 			method: "post",
 			body: JSON.stringify({
 				username,
@@ -14,11 +40,19 @@ async function signupFormHandler(event) {
 				password,
 			}),
 			headers: { "Content-Type": "application/json" },
-		}).then((response) => {
-			console.log(response);
 		});
+		console.log(response);
+		if (response.ok) {
+			console.log("success");
+		} else {
+			alert(response.statusText);
+		}
 	}
 }
+
+document
+	.querySelector(".login-form")
+	.addEventListener("submit", loginFormHandler);
 
 document
 	.querySelector(".signup-form")

7a275c0a134f3e69c08e79201bd69b5f9a557344
Mark Granade <88593585+MarkGranade@users.noreply.github.com>
2021-12-14 12:34:32 -0500

    conditionally render comment form & login/logout buttons

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,7 +31,10 @@ router.get("/", (req, res) => {
 			// loop over and map each Sequelize object into a serialized version of itself, saving the results in a new 'posts' array
 			const posts = dbPostData.map((post) => post.get({ plain: true }));
 			// pass a single post object into the homepage template
-			res.render("homepage", { posts });
+			res.render("homepage", {
+				posts,
+				loggedIn: req.session.loggedIn,
+			});
 		})
 		.catch((err) => {
 			console.log(err);
@@ -80,7 +83,10 @@ router.get("/post/:id", (req, res) => {
 			const post = dbPostData.get({ plain: true });
 
 			// pass data to template
-			res.render("single-post", { post });
+			res.render("single-post", {
+				post,
+				loggedIn: req.session.loggedIn,
+			});
 		})
 		.catch((err) => {
 			console.log(err);
